d5707a8ebb3707e7ec3b3bae900b5a842736524c
Teo Welton <teowelton@gmail.com>
2024-01-30 13:38:24 -0700

    Change redirect URI

--- js/spotifyScript.js
+++ js/spotifyScript.js
@@ -41,9 +41,10 @@ prevButton.addEventListener('click', () => {
 // ------ On LoginButton Click ------ //
 spotifyLoginButton.addEventListener('click', () => {
     const clientId = '73187b57c897429ab8688f1b927b03a6';
+    const redirectURI = 'https://dragondev07.github.io/FocusPomodoro'
 
     // TODO: Update Redirect URI
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:5500&scope=streaming%20user-read-email%20user-read-private`;
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectURI}&scope=streaming%20user-read-email%20user-read-private`;
 });
 
 // ------ On Load ------ //
