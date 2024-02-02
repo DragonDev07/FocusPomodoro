ff937e1d512e87f27eb067819a0b375ea607f95a
Teo Welton <teowelton@gmail.com>
2024-02-01 18:17:55 -0700

    wrong redirect URL

--- js/SpotifyWidget.js
+++ js/SpotifyWidget.js
@@ -41,7 +41,7 @@ prevButton.addEventListener('click', () => {
 // ------ On LoginButton Click ------ //
 spotifyLoginButton.addEventListener('click', () => {
     const clientId = '73187b57c897429ab8688f1b927b03a6';
-    const redirectURI = 'https://dragondev07.github.io/FocusPomodoro/'
+    const redirectURI = 'https://dragondev07.github.io/FocusPomodoro'
 
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectURI}&scope=streaming%20user-read-email%20user-read-private`;
 });
